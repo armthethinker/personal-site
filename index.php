334b068414dec6a683fa6be53d368f3ee874caf9
Andrew R McHugh <arm5629@gmail.com>
2016-01-30 18:01:43 +0000

    Renamed .hero to .homepage-hero

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include($path_includes.'/header.php');
    </div>
    <div class="row">
       <div class="col-lg-5 col-md-6 col-xs-12">
-         <div class="hero">
+         <div class="homepage-hero">
             <h1>Andrew R McHugh</h1>
             <h3>Designer of Futures</h3>
             <p>Sample text. My background is in philosophy, design, how technology and people come together, math, and physics. Along the way, I've built a bikeshare program, worked for systematic sustainability change, codesigned and cobuilt a couple of web apps, cofounded a startup, and did research with sample text sample.</p>
