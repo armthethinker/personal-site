4ea03eb6127f1820c48477c307a8bb2b059fb5d7
Andrew R McHugh <arm5629@gmail.com>
2016-01-30 20:26:13 +0000

    removed large image auto load

--- projects/index.php
+++ projects/index.php
@@ -47,7 +47,7 @@ include($path_includes.'/header.php');
          <div class="showcase-item v2">
             <div class="row">
                <div class="col-lg-8 col-md-7 col-xs-12">
-                  <div style="background-image: url(&quot;https://dl.dropboxusercontent.com/u/32463552/Dasein%20Design%20Public/Codepen/DSC_0002.JPG&quot;)" class="showcase-img"></div>
+                  <div class="showcase-img"></div>
                </div>
                <div class="col-lg-4 col-md-5 col-md-offset-0 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
                   <div class="showcase-description">
