59533c2fea2636adab72cfaad8f3b733f319e3f4
Andrew R McHugh <arm5629@gmail.com>
2016-01-31 17:15:14 +0000

    deleted extraneous code

--- perch/templates/pages/project.php
+++ perch/templates/pages/project.php
@@ -10,22 +10,4 @@
 	<?php perch_content('Description'); ?>
    <?php perch_content('Image Wall'); ?>
 </div>
-<?php
-//<div class="container palette20 palette15-t">
-//	<div class="row m-b-md">
-//	  <div class="col-xs-12">
-//		  <h3>Related Projects</h3>
-//		  <div class="row">
-//				<phpz
-//				  $projectNum=1;
-//				  while ( $projectNum<= 3) {
-//                  include ($path_includes.'/t-project.php');
-//                  $projectNum++;
-//				  }
-//				>
-//			 </div>
-//	  </div>
-//	</div>
-//</div>
-?>
 <?php include($path_includes.'/footer.php');?>
