8243816629df53114e780b0c5c6eb5f27fb40ded
Andrew R McHugh <arm5629@gmail.com>
2016-01-31 17:07:13 +0000

    Removed extraneous code

--- projects/index.php
+++ projects/index.php
@@ -34,21 +34,6 @@ include($path_includes.'/header.php');
                // 'value'=>true
             ));
          ?>
-         <div class="showcase-item hidden">
-            <div class="row">
-               <div class="col-lg-8 col-md-7 col-xs-12">
-                  <div class="showcase-img"></div>
-               </div>
-               <div class="col-lg-4 col-md-5 col-md-offset-0 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
-                  <div class="showcase-description">
-                     <h2>Moral Decision-Making and Haptic Feedback in Virtual Environments</h2>
-                     <h3>Virtual Reality & Moral Experiments</h3>
-                     <p>Moral questions are often posed in writing or conversation. We created a virtual environment to test the Trolley Problem. Using VR, we were able to situate participants closer to a realistic experience and thereby a realistic response.</p>
-                     <div class="btn btn-sm btn-default btn-outline">View Project</div>
-                  </div>
-               </div>
-            </div>
-         </div>
       </div>
    </div>
 </div>
