30c99b29a622fcbc2b4c35cdd72668b0e74a966d
Andrew R McHugh <arm5629@gmail.com>
2016-02-01 16:38:52 +0000

    wrapped showcase perch_content

--- perch/templates/pages/showcase.php
+++ perch/templates/pages/showcase.php
@@ -5,7 +5,11 @@
    include($path_includes.'/header.php');
 ?>
 <div class="container">
-   <?php perch_content('Showcase Meta'); ?>
-   <?php perch_content('Showcase Description'); ?>
+   <div class="row pd-container">
+      <div class="col-xs-12">
+         <?php perch_content('Showcase Meta'); ?>
+         <?php perch_content('Showcase Description'); ?>
+      </div>
+   </div>
 </div>
 <?php include($path_includes.'/footer.php');?>
