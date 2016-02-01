b13cd62df9ac49ff140e2b8246a32362d1791502
Andrew R McHugh <arm5629@gmail.com>
2016-02-01 15:59:12 +0000

    Moved meta tags into Perch attributes file

--- includes/header.php
+++ includes/header.php
@@ -16,25 +16,7 @@
 
    <title><?= $m_title ?></title>
    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-
-   <!--  Meta Meta  -->
-   <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta name="description" content="A lightweight web design company that focuses on how everything interacts. Design for the embedded environment." />
-   <meta name="keywords" content="web design, Columbia, MO, Missouri, web development, flat design, user interface, user experience, layout, " />
-   -->
-   <!--  Schema Tags  -->
-   <!--<meta itemprop="name" content="Dasein Design">
-   <meta itemprop="description" content="A lightweight web design company that focuses on how everything interacts. Design for the embedded environment.">
-   <meta itemprop="image" content="http://daseindesign.co/img/logo-sq.png">
-   -->
-   <!--  Open Graph Tags  -->
-   <!--<meta property="og:title" content="Dasein Design" />
-   <meta property="og:site_name" content="Dasein Design"/>
-   <meta property="og:url" content="http://daseindesign.co" />
-   <meta property="og:description" content="A lightweight web design company that focuses on how everything interacts. Design for the embedded environment." />
-   <meta property="og:image" content="/img/logo-sq.png" />
-   <meta property="fb:app_id" content="1445828285671294" />
-   <meta property="og:type" content="website" />-->
+   <?php perch_page_attributes(); ?>
 
    <?php
       if ($old==true){
