d815ff1fcada46ba5641b9d67462cce6b9d62945
Andrew R McHugh <arm5629@gmail.com>
2016-02-08 14:00:54 +0000

    restructured JS file to be more efficient

--- dist/js/andrewrmchughrocks.js
+++ dist/js/andrewrmchughrocks.js
@@ -1,56 +1,67 @@
+/**
+ * Copyright (c) 2007-2015 Ariel Flesler - aflesler<a>gmail<d>com | http://flesler.blogspot.com
+ * Licensed under MIT
+ * @author Ariel Flesler
+ * @version 2.1.2
+ */
+;(function(f){"use strict";"function"===typeof define&&define.amd?define(["jquery"],f):"undefined"!==typeof module&&module.exports?module.exports=f(require("jquery")):f(jQuery)})(function($){"use strict";function n(a){return!a.nodeName||-1!==$.inArray(a.nodeName.toLowerCase(),["iframe","#document","html","body"])}function h(a){return $.isFunction(a)||$.isPlainObject(a)?a:{top:a,left:a}}var p=$.scrollTo=function(a,d,b){return $(window).scrollTo(a,d,b)};p.defaults={axis:"xy",duration:0,limit:!0};$.fn.scrollTo=function(a,d,b){"object"=== typeof d&&(b=d,d=0);"function"===typeof b&&(b={onAfter:b});"max"===a&&(a=9E9);b=$.extend({},p.defaults,b);d=d||b.duration;var u=b.queue&&1<b.axis.length;u&&(d/=2);b.offset=h(b.offset);b.over=h(b.over);return this.each(function(){function k(a){var k=$.extend({},b,{queue:!0,duration:d,complete:a&&function(){a.call(q,e,b)}});r.animate(f,k)}if(null!==a){var l=n(this),q=l?this.contentWindow||window:this,r=$(q),e=a,f={},t;switch(typeof e){case "number":case "string":if(/^([+-]=?)?\d+(\.\d+)?(px|%)?$/.test(e)){e= h(e);break}e=l?$(e):$(e,q);case "object":if(e.length===0)return;if(e.is||e.style)t=(e=$(e)).offset()}var v=$.isFunction(b.offset)&&b.offset(q,e)||b.offset;$.each(b.axis.split(""),function(a,c){var d="x"===c?"Left":"Top",m=d.toLowerCase(),g="scroll"+d,h=r[g](),n=p.max(q,c);t?(f[g]=t[m]+(l?0:h-r.offset()[m]),b.margin&&(f[g]-=parseInt(e.css("margin"+d),10)||0,f[g]-=parseInt(e.css("border"+d+"Width"),10)||0),f[g]+=v[m]||0,b.over[m]&&(f[g]+=e["x"===c?"width":"height"]()*b.over[m])):(d=e[m],f[g]=d.slice&& "%"===d.slice(-1)?parseFloat(d)/100*n:d);b.limit&&/^\d+$/.test(f[g])&&(f[g]=0>=f[g]?0:Math.min(f[g],n));!a&&1<b.axis.length&&(h===f[g]?f={}:u&&(k(b.onAfterFirst),f={}))});k(b.onAfter)}})};p.max=function(a,d){var b="x"===d?"Width":"Height",h="scroll"+b;if(!n(a))return a[h]-$(a)[b.toLowerCase()]();var b="client"+b,k=a.ownerDocument||a.document,l=k.documentElement,k=k.body;return Math.max(l[h],k[h])-Math.min(l[b],k[b])};$.Tween.propHooks.scrollLeft=$.Tween.propHooks.scrollTop={get:function(a){return $(a.elem)[a.prop]()}, set:function(a){var d=this.get(a);if(a.options.interrupt&&a._last&&a._last!==d)return $(a.elem).stop();var b=Math.round(a.now);d!==b&&($(a.elem)[a.prop](b),a._last=this.get(a))}};return p});
 // Document functions
-$(document).ready(function() {
 
-   // FUNCTION Set the showcase item height
-   function setShowcaseItemHeight() {
-      $('.showcase-item').each(function(i) {
-         //console.log('i: ' + i + ' ' + this);
-         var xWindow = $(window).width();
-         var yImg = $(this).find('.showcase-img').height();
-         if (xWindow >= 992) { //EDIT error at 992 px that is resolved at 993
-            var yShowcaseItem = yImg + 50;
-         } else {
-            //console.log(this);
-            var yShowcaseDescription = $(this).find('.showcase-description').height();
-            var yShowcaseItem = yImg + yShowcaseDescription;
-            //console.log('yImg: ' + yImg + '; yShowcaseDescription: ' + yShowcaseDescription + '; yShowcaseItem: ' + yShowcaseItem);
-         }
-         $(this).height(yShowcaseItem);
-      });
-   }
+// FUNCTION Set the wide img width to 100% of window
+function setImgWideWidth() {
+   $('.pd-img-wide').each(function(i) {
+      //console.log('i: ' + i + ' ' + this);
+      var xWindow = $(window).width();
+      var xImg = xWindow;
+      var xImgOffset = $(this).offset();
+      var xImgOffsetLeft = xImgOffset.left;
+      $(this).offset({ left: 0 }).width(xImg);
+   });
+}
+// FUNCTION Set the showcase item height
+function setShowcaseItemHeight() {
+   $('.showcase-item').each(function(i) {
+      //console.log('i: ' + i + ' ' + this);
+      var xWindow = $(window).width();
+      var yImg = $(this).find('.showcase-img').height();
+      if (xWindow >= 992) { //EDIT error at 992 px that is resolved at 993
+         var yShowcaseItem = yImg + 50;
+      } else {
+         //console.log(this);
+         var yShowcaseDescription = $(this).find('.showcase-description').height();
+         var yShowcaseItem = yImg + yShowcaseDescription;
+         //console.log('yImg: ' + yImg + '; yShowcaseDescription: ' + yShowcaseDescription + '; yShowcaseItem: ' + yShowcaseItem);
+      }
+      $(this).height(yShowcaseItem);
+   });
+}
+// FUNCTION (prototyping) Change active state on nav //edit
+function setNavActive(click){
+   $(click).closest('.nav-items').find('.nav-item').removeClass('active');
+   $(click).addClass('active');
+   return false;
+}
 
+$(document).ready(function(){
    // On load and window resize, reset the showcase item height
    setShowcaseItemHeight();
    $(window).resize(function() {
       setShowcaseItemHeight();
    });
 
-
-   // FUNCTION (prototyping) Change active state on nav //edit
-   function setNavActive(click){
-      $(click).closest('.nav-items').find('.nav-item').removeClass('active');
-      $(click).addClass('active');
-      return false;
-   }
+   // Prototyping nav
    $('.nav-top').on('click', '.nav-item', function(){
       setNavActive(this);
    });
 
-   // FUNCTION Set the wide img width to 100% of window
-   function setImgWideWidth() {
-      $('.pd-img-wide').each(function(i) {
-         //console.log('i: ' + i + ' ' + this);
-         var xWindow = $(window).width();
-         var xImg = xWindow;
-         var xImgOffset = $(this).offset();
-         var xImgOffsetLeft = xImgOffset.left;
-         $(this).offset({ left: 0 }).width(xImg);
+
    // Scroll to
    $('.js-to-top').on('click', function(){
       $(window).scrollTo($('#top'), 800,{
          interrupt: true
       });
-   }
+   });
+
 
    // On load and window resize, reset the img width to 100% of window
    setImgWideWidth();
--- js/andrewrmchughrocks.js
+++ js/andrewrmchughrocks.js
@@ -1,57 +1,60 @@
 // Document functions
-$(document).ready(function() {
 
-   // FUNCTION Set the showcase item height
-   function setShowcaseItemHeight() {
-      $('.showcase-item').each(function(i) {
-         //console.log('i: ' + i + ' ' + this);
-         var xWindow = $(window).width();
-         var yImg = $(this).find('.showcase-img').height();
-         if (xWindow >= 992) { //EDIT error at 992 px that is resolved at 993
-            var yShowcaseItem = yImg + 50;
-         } else {
-            //console.log(this);
-            var yShowcaseDescription = $(this).find('.showcase-description').height();
-            var yShowcaseItem = yImg + yShowcaseDescription;
-            //console.log('yImg: ' + yImg + '; yShowcaseDescription: ' + yShowcaseDescription + '; yShowcaseItem: ' + yShowcaseItem);
-         }
-         $(this).height(yShowcaseItem);
-      });
-   }
+// FUNCTION Set the wide img width to 100% of window
+function setImgWideWidth() {
+   $('.pd-img-wide').each(function(i) {
+      //console.log('i: ' + i + ' ' + this);
+      var xWindow = $(window).width();
+      var xImg = xWindow;
+      var xImgOffset = $(this).offset();
+      var xImgOffsetLeft = xImgOffset.left;
+      $(this).offset({ left: 0 }).width(xImg);
+   });
+}
+// FUNCTION Set the showcase item height
+function setShowcaseItemHeight() {
+   $('.showcase-item').each(function(i) {
+      //console.log('i: ' + i + ' ' + this);
+      var xWindow = $(window).width();
+      var yImg = $(this).find('.showcase-img').height();
+      if (xWindow >= 992) { //EDIT error at 992 px that is resolved at 993
+         var yShowcaseItem = yImg + 50;
+      } else {
+         //console.log(this);
+         var yShowcaseDescription = $(this).find('.showcase-description').height();
+         var yShowcaseItem = yImg + yShowcaseDescription;
+         //console.log('yImg: ' + yImg + '; yShowcaseDescription: ' + yShowcaseDescription + '; yShowcaseItem: ' + yShowcaseItem);
+      }
+      $(this).height(yShowcaseItem);
+   });
+}
+// FUNCTION (prototyping) Change active state on nav //edit
+function setNavActive(click){
+   $(click).closest('.nav-items').find('.nav-item').removeClass('active');
+   $(click).addClass('active');
+   return false;
+}
 
+$(document).ready(function(){
    // On load and window resize, reset the showcase item height
    setShowcaseItemHeight();
    $(window).resize(function() {
       setShowcaseItemHeight();
    });
 
-
-   // FUNCTION (prototyping) Change active state on nav //edit
-   function setNavActive(click){
-      $(click).closest('.nav-items').find('.nav-item').removeClass('active');
-      $(click).addClass('active');
-      return false;
-   }
+   // Prototyping nav
    $('.nav-top').on('click', '.nav-item', function(){
       setNavActive(this);
    });
 
-   // FUNCTION Set the wide img width to 100% of window
-   function setImgWideWidth() {
-      $('.pd-img-wide').each(function(i) {
-         //console.log('i: ' + i + ' ' + this);
-         var xWindow = $(window).width();
-         var xImg = xWindow;
-         var xImgOffset = $(this).offset();
-         var xImgOffsetLeft = xImgOffset.left;
-         $(this).offset({ left: 0 }).width(xImg);
 
    // Scroll to
    $('.js-to-top').on('click', function(){
       $(window).scrollTo($('#top'), 800,{
          interrupt: true
       });
-   }
+   });
+
 
    // On load and window resize, reset the img width to 100% of window
    setImgWideWidth();
