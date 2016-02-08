71c6513a02c0f12f0aab0f8c4e7f1985da0c6c27
Andrew R McHugh <arm5629@gmail.com>
2016-02-08 15:56:24 +0000

    Added link to name

--- includes/nav-top.php
+++ includes/nav-top.php
@@ -1,6 +1,8 @@
 <div class="row nav-top">
    <div class="col-sm-6 col-xs-12">
-      <div class="name">Andrew R McHugh</div>
+      <div class="name">
+         <a href="/">Andrew R McHugh</a>
+      </div>
    </div>
    <div class="col-sm-6 col-xs-12">
       <?php include($path_includes.'/nav-main.php'); ?>
