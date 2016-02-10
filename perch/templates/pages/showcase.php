f355fd557570b319ea48a4a3dfbd8f8356a4d469
Andrew R McHugh <arm5629@gmail.com>
2016-02-10 15:14:05 +0000

    Added previous/next to projects

--- perch/templates/pages/showcase.php
+++ perch/templates/pages/showcase.php
@@ -12,6 +12,25 @@
          <?php perch_content('Showcase Description'); ?>
       </div>
    </div>
+   <div class="pd-paginator">
+      <div class="row">
+         <div class="col-xs-6">
+            <div class="pd-paginator-previous">
+               <div class="pd-paginator-content">
+                  <?php perch_pages_previous_page(array('template' => 'projects-footer-previous.html')); ?>
+               </div>
+            </div>
+         </div>
+         <div class="col-xs-6 text-right">
+            <div class="pd-paginator-previous">
+               <div class="pd-paginator-content">
+                  <?php $page_next = perch_pages_next_page(array('template' => 'projects-footer-next.html')); ?>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+
    <a class="u-back-to-top js-to-top">Back to Top</a>
 </div>
 <?php include($path_includes.'/footer.php');?>
