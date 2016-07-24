6cca40f2346a6a363972669b28829e88643ce8a1
Andrew R McHugh <arm5629@gmail.com>
2016-07-24 16:23:23 -0500

    Added footer and footer styling

--- includes/footer.php
+++ includes/footer.php
@@ -1,21 +1,19 @@
 
-   <footer class="palette19 palette16-t p-tb-md hidden"> <!-- //edit -->
+   <footer class="palette19 palette16-t p-tb-md"> <!-- //edit -->
       <div class="container">
          <div class="row">
-            <div class="col-xs-6">
+            <div class="col-sm-6">
                <i class="fa fa-copyright"></i> Andrew R McHugh
             </div>
-            <div class="col-xs-6">
-               <div class="connect-links pull-right">
-                  <a href="http://be.net/armthethinker"><i class="fa fa-behance-square"></i></a>
+            <div class="col-sm-6">
+               <div class="connect-links">
                   <a href="https://www.facebook.com/arm5629"><i class="fa fa-facebook"></i></a>
                   <a href="http://instagram.com/armthethinker"><i class="fa fa-instagram"></i></a>
                   <a href="http://codepen.io/armthethinker"><i class="fa fa-codepen"></i></a>
-                  <a href="https://plus.google.com/+AndrewMcHughARM5629"><i class="fa fa-google-plus-square"></i></a>
                   <a href="http://www.linkedin.com/pub/andrew-mchugh/48/72b/3ab/"><i class="fa fa-linkedin-square"></i></a>
                   <a href="https://soundcloud.com/andrew-r-mchugh"><i class="fa fa-soundcloud"></i></a>
-                  <a href="http://twitter.com/armthethinker"><i class="fa fa-twitter-square"></i></a>
-                  <a href="https://www.youtube.com/user/arm156291"><i class="fa fa-youtube-square"></i></a>
+                  <a href="http://twitter.com/armthethinker"><i class="fa fa-twitter"></i></a>
+                  <a href="https://www.youtube.com/user/arm156291"><i class="fa fa-youtube"></i></a>
                   <a href="http://open.spotify.com/user/arm5629"><i class="fa fa-spotify"></i></a>
                   <a href="http://medium.com/@armthethinker"><i class="fa fa-medium"></i></a>
                </div>
