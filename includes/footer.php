128d988269b73a5ec9dfbcf661da49093332ff88
Andrew R McHugh <arm5629@gmail.com>
2016-09-11 13:12:46 +0100

    Center the copyright at xs

--- includes/footer.php
+++ includes/footer.php
@@ -2,23 +2,23 @@
    <footer class="palette19 palette16-t p-tb-md"> <!-- //edit -->
       <div class="container">
          <div class="row">
-            <div class="col-sm-6">
-               <i class="fa fa-copyright"></i> Andrew R McHugh
-            </div>
-            <div class="col-sm-6">
+            <div class="col-sm-6 col-sm-push-6 col-xs-12">
                <div class="connect-links">
                   <a href="http://www.linkedin.com/pub/andrew-mchugh/48/72b/3ab/"><i class="fa fa-linkedin-square"></i></a>
                   <a href="https://www.facebook.com/arm5629"><i class="fa fa-facebook"></i></a>
                   <a href="http://twitter.com/armthethinker"><i class="fa fa-twitter"></i></a>
                   <a href="http://instagram.com/armthethinker"><i class="fa fa-instagram"></i></a>
                   <a href="http://codepen.io/armthethinker"><i class="fa fa-codepen"></i></a>
-      
+
                   <a href="http://github.com/armthethinker"><i class="fa fa-github"></i></a>
                   <a href="https://soundcloud.com/andrew-r-mchugh"><i class="fa fa-soundcloud"></i></a>
                   <a href="https://www.youtube.com/user/arm156291"><i class="fa fa-youtube"></i></a>
                   <a href="http://open.spotify.com/user/arm5629"><i class="fa fa-spotify"></i></a>
                   <a href="http://medium.com/@armthethinker"><i class="fa fa-medium"></i></a>
                </div>
+            </div>
+            <div class="col-sm-6 col-sm-pull-6 col-xs-12 copyright">
+               <i class="fa fa-copyright"></i> Andrew R McHugh
             </div>
          </div>
       </div>
