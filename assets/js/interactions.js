54b30f4e7887af4592ece26f510fc9f2cf8ee8f0
Andrew R McHugh <arm5629@gmail.com>
2019-07-19 21:41:32 -0700

    Removed error so JS wouldn't listen for elements that aren't there

--- assets/js/interactions.js
+++ assets/js/interactions.js
@@ -4,13 +4,15 @@ if (is.desktop()){
       perspective: 1500
    })
    const element =  document.querySelector('.animated-hand')
-   element.addEventListener('mouseenter', function() {
-      element.classList.remove('jackInTheBox', 'delay-1s', 'animated')
-      element.classList.add('animated', 'tada')
-   })
-   element.addEventListener('mouseout', function() {
-      element.classList.remove('animated', 'tada')
-   })
+   if (element != null){
+      element.addEventListener('mouseenter', function() {
+         element.classList.remove('jackInTheBox', 'delay-1s', 'animated')
+         element.classList.add('animated', 'tada')
+      })
+      element.addEventListener('mouseout', function() {
+         element.classList.remove('animated', 'tada')
+      })
+   }
 }
 
 function flipTimelineButton(){
